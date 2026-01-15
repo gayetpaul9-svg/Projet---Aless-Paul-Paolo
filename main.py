5b0da82c3d4a44e225edeb64446f333f3e0bb7cc
Patch3108 <poalogilotta@gmail.com>
2026-01-15 13:51:48 +0100

    aaa

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 # from pydoc import text
 #import algo.py
 import pygame 
+from pytmx.util_pygame import load_pygame
+
 running = True
 pygame.init()
 colore=(255, 255, 255)
 coloree=True
 # Crée une fenêtre en plein écran
 screen = pygame.display.set_mode()
+
+tmx_data = load_pygame("map B300 x2.tmx")
+TILE_SIZE = tmx_data.tileheight
+
+
 clock = pygame.time.Clock()
 # Définir la police et le texte à afficherSSS
 font = pygame.font.SysFont(None, 36)
@@ -136,6 +143,13 @@ menu_deroulant= Dropdown(50, 50, 250, 40, ["A200","A201","A202","A203","A204","A
 
 while running:
     screen.fill(colore)
+    
+    # Draw the map tiles
+    for layer in tmx_data.visible_layers:
+        if hasattr(layer, "tiles"):
+            for x, y, image in layer.tiles():
+                screen.blit(image, (x * TILE_SIZE, y * TILE_SIZE))
+    
     menu_deroulant.draw(screen, font)
         
     # Gérer les événements
@@ -147,12 +161,12 @@ while running:
                 running = False
         elif e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
             menu_deroulant.handle_click(e.pos)
-
+    
     menu_deroulant.survol(pygame.mouse.get_pos())
     if coloree == True:
         colore=(255, 255, 255)
     else:
-        colore=(0,0,0) 
+        colore=(255,255,255) 
+    
     pygame.display.flip()
-    clock.tick(60)   
-pygame.quit()
+    clock.tick(60)
