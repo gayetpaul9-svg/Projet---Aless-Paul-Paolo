0574bca6d1b55e647a0c55abbf2a7be6bb756b2b
Patch3108 <poalogilotta@gmail.com>
2026-01-15 13:51:26 +0100

    map

---
+++ automap-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="jeu tuiles 2" tilewidth="2" tileheight="2" tilecount="3" columns="3">
+ <image source="jeu tuiles 2.png" width="6" height="2"/>
+</tileset>
