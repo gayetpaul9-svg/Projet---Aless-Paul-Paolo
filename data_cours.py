cce9c771c68fa2fbaee5a8662bfd06cc986684e7
Paulll <gayetpaul9@gmail.com>
2026-03-12 14:46:12 +0100

    Update data_cours.py

---
+++ data_cours.py
@@ -0,0 +1,41 @@
+ennui_par_cours = {
+    "maths": 65,
+    "physique-chimie": 55,
+    "francais": 80,
+    "histoire-geo": 85,
+    "hggsp": 55,
+    "hlp": 65,
+    "ses": 60,
+    "nsi": 30,
+    "sport": 5,
+    "italien": 65,
+    "anglais": 65,
+    "allemand": 65,
+    "espagnol": 65,
+    "russe": 65,
+    "fls": 80,
+    "cdm": 60,
+    "histoire-geo si": 99,
+    "chinois": 65
+}
+
+messages_par_cours = {
+    "maths": "C'est pas mal mais force à toi",
+    "physique-chimie": "La seule réaction chimique ici c'est ton cerveau qui fond",
+    "francais": "Même Molière se serait endormi",
+    "histoire-geo": "Des dates, des dates, encore des dates... et toi t'as même pas de rendez-vous",
+    "hggsp": "La géopolitique mondiale et toi vous avez un point commun : personne comprend rien",
+    "hlp": "Philosopher c'est bien, dormir c'est mieux",
+    "ses": "L'offre de sommeil est supérieure à la demande de concentration",
+    "nsi": "print('je veux rentrer chez moi') sauf si c'est M.Barthelemy",
+    "sport": "Enfin une heure où rigoler est socialement acceptable",
+    "italien": "Mamma mia que c'est long",
+    "anglais": "Oh my god... are we done yet ?",
+    "allemand": "Schlafen... das ist gut",
+    "espagnol": "Ay dios mio por favor termina ya",
+    "russe": "даже гугл переводчик сдался",
+    "fls": "Lire pour lire pour lire pour lire...",
+    "cdm": "Connaître le monde c'est bien, pouvoir en sortir c'est mieux",
+    "histoire-geo si": "99/100 de taux d'ennui. Le pourcent restant c'est quand tu regardes l'heure",
+    "chinois": "连中国人都说你的发音很有趣"
+}
