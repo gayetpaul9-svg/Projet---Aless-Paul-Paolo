15c6bc10530f84aad0d16c02376c8e8c789272ec
Paulll <gayetpaul9@gmail.com>
2026-04-07 18:42:02 +0200

    Update data_cours.py

--- data_cours.py
+++ data_cours.py
@@ -1,9 +1,3 @@
-"""
-Dictionnaires de métadonnées des cours.
-- ennui_par_cours: niveau d'ennui estimé (0-100).
-- messages_par_cours: message associé au cours.
-"""
-
 ennui_par_cours = {
     "maths": 65,
     "physique-chimie": 55,
@@ -26,22 +20,94 @@ ennui_par_cours = {
 }
 
 messages_par_cours = {
-    "maths": "C'est pas mal mais force à toi",
-    "physique-chimie": "La seule réaction chimique ici\n c'est ton cerveau qui fond",
-    "francais": "Même Molière se serait endormi",
-    "histoire-geo": "Des dates, des dates, encore des dates...\n et toi t'as même pas de rendez-vous",
-    "hggsp": "La géopolitique mondiale et toi vous avez un point commun :\n personne comprend rien",
-    "hlp": "Philosopher c'est bien, dormir c'est mieux",
-    "ses": "L'offre de sommeil est supérieure à\n la demande de concentration",
-    "nsi": "print('je veux rentrer chez moi')\n sauf si c'est M.Barthelemy",
-    "sport": "Enfin une heure où rigoler est\n socialement acceptable",
-    "italien": "Mamma mia que c'est long",
-    "anglais": "Oh my god... are we done yet ?",
-    "allemand": "Schlafen... das ist gut",
-    "espagnol": "Ay dios mio por favor termina ya",
-    "russe": "даже гугл переводчик сдался",
-    "fls": "Lire pour lire pour lire pour lire...",
-    "cdm": "Connaître le monde c'est bien,\n pouvoir en sortir c'est mieux",
-    "histoire-geo si": "99/100 de taux d'ennui.\n Le pourcent restant c'est quand tu regardes l'heure",
-    "chinois": "六十七"
+    "maths": [
+        "C'est pas mal mais force à toi",
+        "x = ennui, résous pour x... \n x = cette heure de cours",
+        "Pythagore est mort depuis 2500 ans \n et il te fait encore souffrir"
+    ],
+    "physique-chimie": [
+        "La seule réaction chimique ici \n c'est ton cerveau qui fond",
+        "E = mc² où m = ton moral\n et c = ce cours",
+        "La physique quantique c'est compliqué,\n mais moins que de rester éveillé"
+    ],
+    "francais": [
+        "Même Molière se serait endormi",
+        "La littérature c'est beau... \nsurtout quand ça se termine",
+        "Analysons le champ lexical\n de l'ennui dans ce cours"
+    ],
+    "histoire-geo": [
+        "Des dates, des dates, encore des dates...\n et toi t'as même pas de rendez-vous",
+        "L'histoire se répète,\n et ce cours aussi",
+        "Napoléon a survécu à Waterloo,\n toi tu survivras à cette heure"
+    ],
+    "hggsp": [
+        "La géopolitique mondiale et toi vous\n avez un point commun : personne comprend rien",
+        "Le monde est en crise, toi aussi",
+        "Entre deux guerres commerciales,\n t'as le temps de somnoler"
+    ],
+    "hlp": [
+        "Philosopher c'est bien,\n dormir c'est mieux",
+        "Descartes a dit je pense donc je suis,\n toi tu penses à partir quand",
+        "La question du sens de la\n vie attendra la sonnerie"
+    ],
+    "ses": [
+        "L'offre de sommeil est supérieure\n à la demande de concentration",
+        "Le PIB de ton attention\n est en récession",
+        "La courbe de ton ennui est\n en croissance exponentielle"
+    ],
+    "nsi": [
+        "print('je veux rentrer chez moi')",
+        "while ennui == True: attendre()",
+        "Erreur 404 : motivation not found"
+    ],
+    "sport": [
+        "Enfin une heure où rigoler\n est socialement acceptable",
+        "La seule heure où tu peux\n courir sans que ça soit bizarre",
+        "Mens sana in corpore\n sano... surtout sano"
+    ],
+    "italien": [
+        "Mamma mia que c'est long",
+        "La dolce vita c'est\n pas vraiment ça",
+        "Même les Italiens feraient la sieste là"
+    ],
+    "anglais": [
+        "Oh my god... are we done yet ?",
+        "To be or not to be... done with this class",
+        "My motivation has left the chat"
+    ],
+    "allemand": [
+        "Schlafen... das ist gut",
+        "Ich bin très fatigué",
+        "Die Motivation ist kaputt"
+    ],
+    "espagnol": [
+        "Ay dios mio por favor termina ya",
+        "No tengo ganas de nada",
+        "La siesta suena muy bien ahora mismo"
+    ],
+    "russe": [
+        "даже гугл переводчик сдался",
+        "я не понимаю ничего и ça va",
+        "кириллица c'est déjà un sport"
+    ],
+    "fls": [
+        "Lire pour lire pour lire pour lire...",
+        "Un livre par semaine,\n un neurone par mois",
+        "La lecture c'est l'évasion...\n mais t'as pas la clé"
+    ],
+    "cdm": [
+        "Connaître le monde c'est bien,\n pouvoir en sortir c'est mieux",
+        "Le monde est grand, ce cours\n l'est encore plus",
+        "Tour du monde en 80 bâillements"
+    ],
+    "histoire-geo si": [
+        "99/100 de taux d'ennui. Le pourcent\n restant c'est quand tu regardes l'heure",
+        "La souffrance a un nom \net ce nom c'est histoire-geo si",
+        "Même le prof a l'air de s'ennuyer"
+    ],
+    "chinois": [
+        "六十七",
+        "我不comprends rien du tout",
+        "4000 caractères à\n apprendre, 0 motivation"
+    ]
 }
