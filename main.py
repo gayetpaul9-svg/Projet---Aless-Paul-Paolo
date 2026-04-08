0819ef35adfa0024767f02dd16fb42bd03fe7650
Alessandro <90963136+aless-09@users.noreply.github.com>
2026-04-08 20:31:34 +0200

    tous les boutons salle sur la carte

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ info = pygame.display.Info()
 
 # Variables globales
 afficher = False
+afficher_s = False
 long=False
 etat = "accueil"
 running = True
@@ -36,6 +37,8 @@ fonc={
     'temps' : "0"
 }
 etage = 5
+departure_room = None
+arrival_room = None
 chemins1B = []
 chemins1 = []
 chemins_cdi = []
@@ -188,6 +191,411 @@ map_buttons_info = [
         'image': 'marque'
     },
 ]
+# boutons_salles: on associe des coordonnée et un étage à chaque bouton
+taille_txt = pygame.font.Font("assets/DejaVuSans.ttf", 18)
+boutons_salles = [
+    {
+        'label': 'A001',
+        'button': classes.Button(offset_x + 402, offset_y + 272, 60, 20, 'A001', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A002',
+        'button': classes.Button(offset_x + 430, offset_y + 722, 60, 20, 'A002', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A003',
+        'button': classes.Button(offset_x + 340, offset_y + 722, 60, 20, 'A003', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A004',
+        'button': classes.Button(offset_x + 340, offset_y + 802, 60, 20, 'A004', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A005',
+        'button': classes.Button(offset_x + 255, offset_y + 722, 60, 20, 'A005', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A006',
+        'button': classes.Button(offset_x + 155, offset_y + 477, 60, 20, 'A006', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A007',
+        'button': classes.Button(offset_x + 155, offset_y + 432, 60, 20, 'A007', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A008',
+        'button': classes.Button(offset_x + 155, offset_y + 332, 60, 20, 'A008', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A009',
+        'button': classes.Button(offset_x + 165, offset_y + 272, 60, 20, 'A009', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A010',
+        'button': classes.Button(offset_x + 15, offset_y + 287, 60, 20, 'A010', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A011',
+        'button': classes.Button(offset_x + 4, offset_y + 352, 60, 20, 'A011', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A012',
+        'button': classes.Button(offset_x + 4, offset_y + 412, 60, 20, 'A012', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A013',
+        'button': classes.Button(offset_x + 4, offset_y + 477, 60, 20, 'A013', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'A014',
+        'button': classes.Button(offset_x + 20, offset_y + 572, 60, 20, 'A014', font=taille_txt),
+        'floors': [1],
+    },
+    {
+        'label': 'B111',
+        'button': classes.Button(offset_x + 206, offset_y + 812, 60, 20, 'B111', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B112',
+        'button': classes.Button(offset_x + 276, offset_y + 717, 60, 20, 'B112', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B113',
+        'button': classes.Button(offset_x + 315, offset_y + 625, 60, 20, 'B113', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B114',
+        'button': classes.Button(offset_x + 166, offset_y + 333, 60, 20, 'B114', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B115',
+        'button': classes.Button(offset_x + 153, offset_y + 192, 60, 20, 'B115', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B116',
+        'button': classes.Button(offset_x + 300, offset_y + 205, 60, 20, 'B116', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B117',
+        'button': classes.Button(offset_x + 407, offset_y + 205, 60, 20, 'B117', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'B118',
+        'button': classes.Button(offset_x + 495, offset_y + 205, 60, 20, 'B118', font=taille_txt),
+        'floors': [2],
+    },
+    {
+        'label': 'A101',
+        'button': classes.Button(offset_x + 429, offset_y + 86, 60, 20, 'A101', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A102',
+        'button': classes.Button(offset_x + 312, offset_y + 199, 60, 20, 'A102', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A103',
+        'button': classes.Button(offset_x + 213, offset_y + 199, 60, 20, 'A103', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A104',
+        'button': classes.Button(offset_x + 117, offset_y + 199, 60, 20, 'A104', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A105',
+        'button': classes.Button(offset_x + 20, offset_y + 199, 60, 20, 'A105', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A106',
+        'button': classes.Button(offset_x + 131, offset_y + 607, 60, 20, 'A106', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A107',
+        'button': classes.Button(offset_x + 28, offset_y + 607, 60, 20, 'A107', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A108',
+        'button': classes.Button(offset_x + 28, offset_y + 737, 60, 20, 'A108', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A109',
+        'button': classes.Button(offset_x + 28, offset_y + 808, 60, 20, 'A109', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A110',
+        'button': classes.Button(offset_x + 28, offset_y + 855, 60, 20, 'A110', font=taille_txt),
+        'floors': [3],
+    },
+    {
+        'label': 'A201',
+        'button': classes.Button(offset_x + 113, offset_y + 345, 60, 20, 'A201', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A202',
+        'button': classes.Button(offset_x + 138, offset_y + 302, 60, 20, 'A202', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A203',
+        'button': classes.Button(offset_x + 180, offset_y + 275, 60, 20, 'A203', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A204',
+        'button': classes.Button(offset_x + 218, offset_y + 302, 60, 20, 'A204', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A205',
+        'button': classes.Button(offset_x + 261, offset_y + 246, 60, 20, 'A205', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A206',
+        'button': classes.Button(offset_x + 261, offset_y + 170, 60, 20, 'A206', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A207',
+        'button': classes.Button(offset_x + 261, offset_y + 52, 60, 20, 'A207', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A208',
+        'button': classes.Button(offset_x + 304, offset_y + 368, 60, 20, 'A208', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A209',
+        'button': classes.Button(offset_x + 304, offset_y + 415, 60, 20, 'A209', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A210',
+        'button': classes.Button(offset_x + 304, offset_y + 457, 60, 20, 'A210', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A211',
+        'button': classes.Button(offset_x + 307, offset_y + 506, 60, 20, 'A211', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A212',
+        'button': classes.Button(offset_x + 394, offset_y + 514, 60, 20, 'A212', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B213',
+        'button': classes.Button(offset_x + 389, offset_y + 654, 60, 20, 'B213', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B214',
+        'button': classes.Button(offset_x + 307, offset_y + 647, 60, 20, 'B214', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B215',
+        'button': classes.Button(offset_x + 254, offset_y + 642, 60, 20, 'B215', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B216',
+        'button': classes.Button(offset_x + 203, offset_y + 690, 60, 20, 'B216', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B217',
+        'button': classes.Button(offset_x + 285, offset_y + 731, 60, 20, 'B217', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B218',
+        'button': classes.Button(offset_x + 352, offset_y + 731, 60, 20, 'B218', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B219',
+        'button': classes.Button(offset_x + 362, offset_y + 783, 60, 20, 'B219', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B220',
+        'button': classes.Button(offset_x + 362, offset_y + 826, 60, 20, 'B220', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B221',
+        'button': classes.Button(offset_x + 362, offset_y + 868, 60, 20, 'B221', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B222',
+        'button': classes.Button(offset_x + 440, offset_y + 914, 60, 20, 'B222', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'B223',
+        'button': classes.Button(offset_x + 320, offset_y + 914, 60, 20, 'B223', font=taille_txt),
+        'floors': [4],
+    },
+    {
+        'label': 'A301',
+        'button': classes.Button(offset_x + 33, offset_y + 52, 60, 20, 'A301', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A302',
+        'button': classes.Button(offset_x + 97, offset_y + 52, 60, 20, 'A302', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A303',
+        'button': classes.Button(offset_x + 155, offset_y + 52, 60, 20, 'A303', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A304',
+        'button': classes.Button(offset_x + 98, offset_y + 90, 60, 20, 'A304', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A305',
+        'button': classes.Button(offset_x + 98, offset_y + 144, 60, 20, 'A305', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A306',
+        'button': classes.Button(offset_x + 98, offset_y + 204, 60, 20, 'A306', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A307',
+        'button': classes.Button(offset_x + 98, offset_y + 263, 60, 20, 'A307', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A308',
+        'button': classes.Button(offset_x + 98, offset_y + 318, 60, 20, 'A308', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A309',
+        'button': classes.Button(offset_x + 203, offset_y + 158, 60, 20, 'A309', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A310',
+        'button': classes.Button(offset_x + 203, offset_y + 246, 60, 20, 'A310', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B311',
+        'button': classes.Button(offset_x + 200, offset_y + 427, 60, 20, 'B311', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'A312',
+        'button': classes.Button(offset_x + 149, offset_y + 530, 60, 20, 'A312', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B313',
+        'button': classes.Button(offset_x + 20, offset_y + 530, 60, 20, 'B313', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B314',
+        'button': classes.Button(offset_x + 175, offset_y + 633, 60, 20, 'B314', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B315',
+        'button': classes.Button(offset_x + 280, offset_y + 705, 60, 20, 'B315', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B316',
+        'button': classes.Button(offset_x + 280, offset_y + 763, 60, 20, 'B316', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B317',
+        'button': classes.Button(offset_x + 280, offset_y + 824, 60, 20, 'B317', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B318',
+        'button': classes.Button(offset_x + 296, offset_y + 889, 60, 20, 'B318', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B319',
+        'button': classes.Button(offset_x + 369, offset_y + 882, 60, 20, 'B319', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B320',
+        'button': classes.Button(offset_x + 430, offset_y + 882, 60, 20, 'B320', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B321',
+        'button': classes.Button(offset_x + 501, offset_y + 882, 60, 20, 'B321', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B322',
+        'button': classes.Button(offset_x + 493, offset_y + 842, 60, 20, 'B322', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B323',
+        'button': classes.Button(offset_x + 435, offset_y + 810, 60, 20, 'B323', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B324',
+        'button': classes.Button(offset_x + 393, offset_y + 842, 60, 20, 'B324', font=taille_txt),
+        'floors': [5],
+    },
+    {
+        'label': 'B325',
+        'button': classes.Button(offset_x + 359, offset_y + 810, 60, 20, 'B325', font=taille_txt),
+        'floors': [5],
+    },
+]
+
 
 # Fermer bouton image
 close_button = classes.Button(screen.get_width() - 50, 10, 40, 40, "X", font=pygame.font.Font("assets/DejaVuSans.ttf", 20))
@@ -217,6 +625,7 @@ menu_deroulant= classes.Dropdown(50, 50, 250, 40, [
     "B321","B322","B323","B324","B325",""
 ])
 afficher_loupe = classes.Button(info.current_w-447+200,info.current_h-735, 200, 50)
+afficher_salles = classes.Button(info.current_w-447+200,info.current_h-535, 200, 50)
 menu_matiere= classes.Dropdown(320,50,250,40, [
     "maths","physique-chimie","francais", "histoire-geo", "hggsp", "hlp",
     "ses", "nsi", "sport", "italien", "anglais", "allemand", "espagnol",
@@ -316,6 +725,10 @@ while running:
                 for info_btn in map_buttons_info:
                     info_btn['button'].clicked = False
 
+            if afficher_s == True:
+                for info_btn in boutons_salles:
+                    info_btn['button'].clicked = False
+
             if bat_a.is_clicked(e.pos):
                 batiment_selectionne = "A"
                 if etage == 2:
@@ -364,6 +777,10 @@ while running:
                 if afficher_loupe.is_clicked(e.pos):
                     afficher = not afficher
                     son_clic.play()
+
+                if afficher_salles.is_clicked(e.pos):
+                    afficher_s = not afficher_s
+                    son_clic.play()
                 
                 # Bouton Options du gps
                 if bouton_options_x <= mx <= bouton_options_x + bouton_options_largeur and bouton_options_y <= my <= bouton_options_y + bouton_options_hauteur:
@@ -380,6 +797,44 @@ while running:
                             btn.clicked = True
                             show_image = True
                             current_image = images[info_btn['image']]
+                            son_clic.play()
+                            break
+
+                if afficher_s == True:
+                    for info_btn in boutons_salles:
+                        btn = info_btn['button']
+                        if etage not in info_btn['floors']:
+                            continue
+                        if btn.is_clicked(e.pos):
+                            if departure_room is None:
+                                departure_room = info_btn['label']
+                                classes.départ_salle = departure_room
+                                print(f"Départ sélectionné : {departure_room}")
+                            elif arrival_room is None:
+                                arrival_room = info_btn['label']
+                                classes.arrivée_salle = arrival_room
+                                print(f"Arrivée sélectionnée : {arrival_room}")
+                                # Trigger GPS calculation
+                                _, chemins = fonctions.gps(classes.départ_salle, classes.arrivée_salle, tmx_data, tmx_data_b, tmx_data_d, tmx_data_c, tmx_data_e, layers_2, layers_3, layers_1, layers_cdi, layers_1B, mode_long)
+                                fonc = fonctions.fonctionnalitees(classes.départ_salle, classes.arrivée_salle)
+                                # Find etage for departure
+                                for layer in layers:
+                                    if classes.départ_salle in layer:
+                                        for cle, v in dico_etage.items():
+                                            if v == layer:
+                                                etage = cle
+                                                break
+                                        break
+                                saisie_active = False
+                                texte_saisi = ""
+                                infos_cours_result = None
+                            else:
+                                # Reset if both are selected and clicked again
+                                departure_room = None
+                                arrival_room = None
+                                classes.départ_salle = None
+                                classes.arrivée_salle = None
+                                chemins = [chemins1, chemins2, chemins3]  # Reset paths
                             son_clic.play()
                             break
 
@@ -446,6 +901,7 @@ while running:
         etage_inf.draw(screen, top_left=0, top_right=0, bottom_right=5, bottom_left=5)
         nom_etage.draw(screen, text=noms_etages[etage], top_left=10, top_right=10, bottom_right=10, bottom_left=10)
         afficher_loupe.draw(screen, text="Afficher loupes", top_left=10, top_right=10, bottom_right=10, bottom_left=10)
+        afficher_salles.draw(screen, text="Afficher salles", top_left=10, top_right=10, bottom_right=10, bottom_left=10)
         if afficher == True:
                         for info_btn in map_buttons_info:
                             btn = info_btn['button']
@@ -453,6 +909,18 @@ while running:
                                 continue
                             color = (0, 255, 0) if btn.clicked else (255, 0, 0)
                             btn.draw(screen, top_left=0, top_right=0, bottom_right=0, bottom_left=0, color=color)
+        if afficher_s == True:
+                        for info_btn in boutons_salles:
+                            btn = info_btn['button']
+                            if etage not in info_btn['floors']:
+                                continue
+                            if info_btn['label'] == departure_room:
+                                color = (0, 255, 0)  # Green for departure
+                            elif info_btn['label'] == arrival_room:
+                                color = (255, 0, 0)  # Red for arrival
+                            else:
+                                color = (200, 200, 200)  # Default light gray
+                            btn.draw(screen, top_left=5, top_right=5, bottom_right=5, bottom_left=5, color=color)
         
     #if etage == 2 and batiment_selectionne == "A":
      #   etage=3
